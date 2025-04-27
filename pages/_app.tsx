26e3dad6e12de83c0e448b01eb31d518bf2934ce
Mikoman5 <aaqualityservice@gmail.com>
2025-04-26 22:03:19 -0400

    deleted _app.tsx

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "./styles/globals.css"; // Relative path works in all projects
-
-import type { AppProps } from "next/app";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
